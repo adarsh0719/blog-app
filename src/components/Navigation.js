0500ca4925d2eea995ae3307054be01432e9b05d
adarsh0719 <adarshreddy247@gmail.com>
2025-05-28 19:14:08 +0530

    navbar changes

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,21 +1,81 @@
-import React from 'react';
-import { Navbar, Container, Nav } from 'react-bootstrap';
-import { Link } from 'react-router-dom';
+import React, { useState } from 'react';
+import { Navbar, Container, Nav, Button } from 'react-bootstrap';
+import { Link, useLocation } from 'react-router-dom';
 
 const Navigation = () => {
+  const [expanded, setExpanded] = useState(false);
+  const location = useLocation();
+  
+  const closeMenu = () => setExpanded(false);
+  
   return (
-    <Navbar bg="dark" variant="dark" expand="lg" sticky="top">
-      <Container>
-        <Navbar.Brand as={Link} to="/">BlogHub</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="ms-auto">
-            <Nav.Link as={Link} to="/">Home</Nav.Link>
-            <Nav.Link as={Link} to="/add-blog" className="btn btn-primary text-white">
-              + Add Blog
-            </Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
+    <Navbar 
+      bg="dark" 
+      variant="dark" 
+      expand="lg" 
+      sticky="top"
+      expanded={expanded}
+      onToggle={(isOpen) => setExpanded(isOpen)}
+    >
+      <Container fluid>
+        <Navbar.Brand as={Link} to="/" className="fw-bold" onClick={closeMenu}>
+          <span className="d-inline-block align-middle px-2">
+            Blog<span className="text-primary">Hub</span>
+          </span>
+        </Navbar.Brand>
+        
+       <div className="d-lg-none ms-auto">
+  <Button 
+    as={Link}
+    to="/add-blog"
+    variant="primary"
+    className="px-2 py-1"
+    onClick={closeMenu}
+  >
+    Add Blog
+  </Button>
+</div>
+
+        
+        <Navbar.Toggle 
+          aria-controls="main-navbar" 
+          className="ms-2 border-0"
+        />
+        
+       <Navbar.Collapse id="main-navbar">
+  <Nav className="w-100 align-items-center">
+
+    <Nav.Link
+      as={Link}
+      to="/"
+      className={`d-lg-none py-3 ${location.pathname === '/' ? 'active' : ''}`}
+      onClick={closeMenu}
+    >
+      Home
+    </Nav.Link>
+
+    <div className="d-none d-lg-flex ms-lg-auto align-items-center gap-3">
+      <Nav.Link
+        as={Link}
+        to="/"
+        className={`${location.pathname === '/' ? 'active' : ''}`}
+        onClick={closeMenu}
+      >
+        Home
+      </Nav.Link>
+
+      <Button
+        as={Link}
+        to="/add-blog"
+        variant="primary"
+        className="px-4 py-2 fw-medium"
+        onClick={closeMenu}
+      >
+        + Add Blog
+      </Button>
+    </div>
+  </Nav>
+</Navbar.Collapse>
       </Container>
     </Navbar>
   );
